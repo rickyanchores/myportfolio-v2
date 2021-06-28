75a78c96fd367f91fde9be2c6a9337704670e040
RickyAnchores <ricky23anchores@gmail.com>
2021-06-28 19:48:01 +0100

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <h1>App</h1>
+      <Navbar />
     </div>
   );
 }
