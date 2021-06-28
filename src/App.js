d834995f11e73a637f1e83dfc42d4bf766998c16
RickyAnchores <ricky23anchores@gmail.com>
2021-06-28 20:27:46 +0100

    first deploy + navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/Navbar/Navbar";
 const App = () => {
   return (
     <div className="App">
-      <h1>App</h1>
       <Navbar />
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,17 @@ import "./Navbar.scss";
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <h1>Navbar</h1>
+            <div className="logo"><h1>Ricky A</h1></div>
+            <div className="hamburger">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+            <ul className="navlinks">
+                <li><a href="">Home</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">About</a></li>
+            </ul>
         </div>
     )
 }
